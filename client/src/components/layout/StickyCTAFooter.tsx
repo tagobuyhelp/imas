56340eee143f1a89aa194a3a3d15afa5b28c67ab
Tagobuy <tagobuyhelp@gmail.com>
2025-11-18 11:54:47 +0530

    ..

--- client/src/components/layout/StickyCTAFooter.tsx
+++ client/src/components/layout/StickyCTAFooter.tsx
@@ -129,7 +129,7 @@ export function StickyCTAFooter({
 
       {/* Floating WhatsApp & Call Buttons */}
       {showWhatsAppButton && (
-        <div className="fixed bottom-4 right-4 z-40 flex flex-col sm:flex-row gap-3">
+        <div className="fixed bottom-28 right-4 z-40 flex flex-col sm:flex-row gap-3">
           {/* WhatsApp Button */}
           <button
             onClick={handleWhatsAppClick}
