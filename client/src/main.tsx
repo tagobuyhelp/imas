c04a528b0bbe5b3d2178a4253cadf48c795a236f
Tagobuy <tagobuyhelp@gmail.com>
2025-11-25 14:11:07 +0530

    ...

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
+import './lib/npfInit';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
